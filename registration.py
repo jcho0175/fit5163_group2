ef5d42f125d7010783a45e2bcc8c720974a17494
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-05-09 15:09:53 +1000

    Improved registration?.

--- registration.py
+++ registration.py
@@ -1,6 +1,10 @@
-from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.primitives import hashes
+# from cryptography import x509
+# from cryptography.x509.oid import NameOID
+# from cryptography.hazmat.primitives import hashes
+from random import Random
+from encrypt_request_Test1 import RSAEncryptorDecryptor
+from uuid import uuid4
+
 
 class Registration:
     # Implement client registration functionality, allowing clients to provide their identity and public key.
@@ -9,26 +13,28 @@ class Registration:
 
     # Validity period, public key, identity from other parts
     def __init__(self, client_id):
-        self.client_id = client_id
-        self.identity = None
+        self.identity = client_id
         self.public_key = None
-        self.validity_period = None
 
-    def register_client(self, identity, public_key):
+    def register_client(self):
 
+        # Identity is something like monash ID, make it unique
+        rsa = RSAEncryptorDecryptor()
+        rsa.generate_key_pair()
+        print("Public Key", rsa.public_key)
         self.clients.append(
             {
-                "id": self.client_id, 
-                "identity": self.identity, 
-                "pub_key": self.public_key
+                "identity": self.identity,
+                "pub_key": rsa.public_key
             }
         )
 
 
+if __name__ == '__main__':
 
-        
-        
-        
+    def main():
+        id = uuid4()
+        registration = Registration(id)
+        registration.register_client()
 
-
-    
+    main()
