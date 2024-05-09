ab79f1e7b991007366820a7231962ec09a4daf4a
jennyChoi <jcho0175@student.monash.edu>
2024-05-09 14:55:36 +1000

    pushing main method

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def start_program(name):
+    """
+    a. Implement functionalities for the root CA to generate its own private/public key pair.
+
+    b. Develop functions for issuing certIficates for clients, including necessary
+    attributes such as client ID, public key, and validity period.
+
+    c. Implement client registratIon functionality, allowing clients to provide their identity and public key.
+
+    d. Develop a mechanism for clients to submit certificate requests to the CA. The request should be encrypted.
+
+    e. Develop methods for validating certificates using the corresponding public keys and CA signatures.
+
+    f. Implement mechanisms for certificate revocation in case of compromise or expiration.
+    """
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    start_program()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
